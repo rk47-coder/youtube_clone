c4515a1ad95a9c1a818e686ce57cc6090b9dcdf6
Raju Kumar <rajukumar150198@gmail.com>
2024-09-27 13:58:30 +0530

    crete route and nvbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Box } from "@mui/material";
+import { ChannelDetail, Feed, Navbar, SearchFeed, VideoDetail } from "./components";
+import React from "react";
 
 const App = () => {
   return (
-    <div>App</div>
+    <BrowserRouter>
+        <Box sx={{backgroundColor: '#000'}}>
+            <Navbar/>
+            <Routes>
+                <Route path="/" exact element={<Feed/>}/>
+                <Route path="/video/:id" element={<VideoDetail/>} />
+                <Route path="/channel/:id" element={<ChannelDetail/>} />
+                <Route path="/search/:searchTerm" element={<SearchFeed/>} />
+            </Routes>
+
+        </Box>
+    </BrowserRouter>
   )
 }
 
---
+++ src/components/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Navbar from "./Navbar";
+import Feed from "./Feed";
+import SearchFeed from "./SearchFeed";
+import VideoDetail from "./VideoDetail";
+import ChannelDetail from "./ChannelDetail";
+
+export { 
+    Navbar,
+    Feed,
+    SearchFeed,
+    VideoDetail,
+    ChannelDetail
+}
