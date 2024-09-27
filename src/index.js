d72bf5a7ff8b7031f83ac2d38e20c60d37884a1a
Raju Kumar <rajukumar150198@gmail.com>
2024-09-27 11:45:36 +0530

    set layout

--- src/index.js
+++ src/index.js
@@ -1,2 +1,9 @@
 import React from "react";
-import ReactDom from "react-dom"
+import ReactDOM from "react-dom/client";
+
+import App from './App';
+import './index.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(<App />);
